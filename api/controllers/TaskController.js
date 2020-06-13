e06781bf8c2c0541f438e906a8255b49624ba492
Iwona <iwona.gorskaa@gmail.com>
2020-06-13 20:16:54 +0200

    Token is working good, changed the way of displaying tasks, form creates new tasks with real data and adds them to the list

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -8,18 +8,31 @@ let moment = require('moment');
 
 module.exports = {
 
-    createTask: async function (req, res) {sa
+    createTask: async function (req, res) {
+        //sa
         let token = req.param('token');
         let user = await UserSession.find({ where: { token: token } });
         user = user[0];
-        let newRecord = await Task.create({
+        /*let newRecord = await Task.create({
+            title: "Title",
             userSessionID: user.id,
             deadlineTime: moment().add(1, 'weeks').toDate().getTime(), //w przyszlosci chyba pobrane od usera
             priority: 3,
             status: 0,
             finished: false,
             task: { foo: 'bar' }
+        }).fetch();*/
+
+        let newRecord = await Task.create({
+            title: req.param('title'),
+            userSessionID: user.id,
+            deadlineTime: moment().add(1, 'weeks').toDate().getTime(), //w przyszlosci chyba pobrane od usera
+            priority: req.param('priority'),
+            status: 0,
+            finished: false,
+            task: req.param('content')
         }).fetch();
+
         return res.ok();
     },
 
